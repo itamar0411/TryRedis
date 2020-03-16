b82e307511a85d4476eda1e27c312d3d2ebeb4ee
itamar0411 <itamar0411@gmail.com>
2020-03-15 21:37:22 -0400

    Added an example for Redis getSet(key, newValue) method which gets the old value and sets the new value for the input key.

--- src/main/java/redis_client/RedisClient.java
+++ src/main/java/redis_client/RedisClient.java
@@ -45,6 +45,11 @@ public class RedisClient {
         Thread.sleep(3000);
         System.out.println("Value for key=itamar2 after it expires: " + jedis.get("itamar2"));
 
+        // Use a method that sets a new value to a key and returns the old value
+        String oldValue = jedis.getSet("amitai", "test5");
+        System.out.println("Old value for amitai was: " + oldValue);
+        System.out.println("New value for amitai is: " + jedis.get("amitai"));
+
         // Delete 3 keys using the del function. you can also usethe unlink function
         // to delete the keys. Using unlink the actual memory reclaiming in a different thread,
         // so it is not blocking, while del is
